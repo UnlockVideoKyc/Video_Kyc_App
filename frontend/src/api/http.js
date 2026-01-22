1ca31db2333bdbb2c7a9b57be8f1f5dfb72fb718
HarshVhkm <harshvhkm123@gmail.com>
2026-01-22 16:40:00 +0530

    fix: fix url issue

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -1,4 +1,7 @@
-const BASE_URL = `${API_BASE_URL}/api/auth`;
+
+
+
+const BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api/auth`;
 
 const apiFetch = async (url, options = {}) => {
   const response = await fetch(`${BASE_URL}${url}`, {
