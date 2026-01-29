a46ad8cac419ba95e65d8190ce06463fdba64992
therahulsharma108 <imvishalsharma312@gmail.com>
2026-01-29 22:00:27 +0530

    fix:role based module

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -1,14 +1,33 @@
 const nodemailer = require("nodemailer");
 
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 465,
+//   secure: true,
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: true, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false   // <-- FIX HERE
+  }
 });
+
+
+
+
+
 
 const sendOTP = async (email, otp) => {
   try {
