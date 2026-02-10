484c998acf7337ecf4374beadf0a5843143c0d6a
HarshVhkm <harshvhkm123@gmail.com>
2026-02-10 11:13:22 +0530

    fix: added v1 health

--- backend/src/app.js
+++ backend/src/app.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
 app.use("/v1", routes);
 
 // Health check
-app.get("/", (req, res) => {
+app.get("/health", (req, res) => {
   res.json({
     success: true,
     message: "DigiKhata Backend API",
